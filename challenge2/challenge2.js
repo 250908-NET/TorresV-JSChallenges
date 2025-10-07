8b6178d9d105031dd6cc16a1b4d69f1291f6af9a
vitugo23 <vitugo7@hotmail.com>
2025-10-07 16:11:48 -0400

    challenge2 done

---
+++ challenge2/challenge2.js
@@ -0,0 +1,16 @@
+let name = "Jordan";
+let age = 25;
+
+console.log("Name:", name);
+console.log("Age:", age);
+
+name = "Alex";
+age = age + 5;
+console.log("Updated age:", age);
+
+//Create variables for your favorite color, movie, and number, then log them all in one sentence.
+
+let color = "Blue";
+let movie = "Matrix";
+let number = 23;
+console.log("Favorite Color: ", color , "Favorite Movie: ", movie , "Lucky Number: ", number);
