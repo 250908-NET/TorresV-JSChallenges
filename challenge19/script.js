0deee90521c0f847dc454a570de242d779e5f0b0
vitugo23 <vitugo7@hotmail.com>
2025-10-09 13:49:59 -0400

    challenge19 done

---
+++ challenge19/script.js
@@ -0,0 +1,8 @@
+//Use the array `.map()` method and an arrow function to create a new array of
+//**squares** of these numbers:
+const nums = [1, 2, 3, 4, 5];
+
+// Use map with arrow function to get squares
+const squares = nums.map(n => n * n);
+
+console.log(squares); // [1, 4, 9, 16, 25]
