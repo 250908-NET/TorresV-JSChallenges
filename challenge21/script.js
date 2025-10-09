d5bf40a1d25e9a3ab77c4854325ad89efcbf5aa1
vitugo23 <vitugo7@hotmail.com>
2025-10-09 13:59:24 -0400

    challenge21 done

---
+++ challenge21/script.js
@@ -0,0 +1,9 @@
+//Write an arrow function named `makeBook` that takes two parameters 
+//— `title` and `author`
+
+// Arrow function that returns an object
+const makeBook = (title, author) => ({ title, author });
+
+// Test it
+console.log(makeBook("Kinito Mendez", "Kiara Calderon"));
+console.log(makeBook("Anacaona", "Viviana Torres"));
